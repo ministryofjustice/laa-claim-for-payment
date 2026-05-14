27fae94a1ba6054acb42b33a6cdbb57fab339443
Adam Coles <adam.coles@justice.gov.uk>
2026-05-14 15:52:51 +0100

    updated civil claims client and local claim model

--- claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/model/Claim.java
+++ claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/model/Claim.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -66,4 +67,12 @@ public class Claim implements Serializable {
   @Schema(description = "id of the submission this claim belongs to")
   @JsonProperty("submissionId")
   private UUID submissionId;
+
+  @Schema(description = "line items associated with the claim")
+  @JsonProperty("lineItems")
+  private List<LineItem> lineItems;
+
+  @JsonProperty("evidence")
+  @Schema(description = "evidence associated with the claim")
+  private List<ClaimEvidence> evidence;
 }
