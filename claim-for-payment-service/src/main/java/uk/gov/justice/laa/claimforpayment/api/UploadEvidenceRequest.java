93a726f67ea45106ae89e1c05d815ff20e2ed229
Adam Coles <adam.coles@justice.gov.uk>
2026-05-19 16:40:59 +0100

    updated file upload spec

---
+++ claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/api/UploadEvidenceRequest.java
@@ -0,0 +1,11 @@
+package uk.gov.justice.laa.claimforpayment.api;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.web.multipart.MultipartFile;
+
+/** Request body for uploading evidence files. */
+public class UploadEvidenceRequest {
+
+  @Schema(type = "string", format = "binary", description = "Evidence file")
+  public MultipartFile file;
+}
