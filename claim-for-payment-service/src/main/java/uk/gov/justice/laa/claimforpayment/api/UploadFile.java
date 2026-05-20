dd8ea80e1d3e482f36bd9bc3f80ca1f5fd6ee59d
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-05-20 14:33:12 +0100

    CLAS-160: Using Long instead of long for file size

--- claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/api/UploadFile.java
+++ claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/api/UploadFile.java
@@ -3,7 +3,7 @@ package uk.gov.justice.laa.claimforpayment.api;
 import org.springframework.web.multipart.MultipartFile;
 
 /** File information for upload operations. */
-public record UploadFile(String filename, String originalname, long filesize) {
+public record UploadFile(String filename, String originalname, Long filesize) {
 
   public UploadFile(MultipartFile file) {
     this(file.getOriginalFilename(), file.getOriginalFilename(), file.getSize());
