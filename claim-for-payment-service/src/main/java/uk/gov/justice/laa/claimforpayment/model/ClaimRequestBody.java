c2ff89832f2ba909b0770b738dff80d664b13d47
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-07-21 09:31:10 +0100

    CLAS-639: Updated schema description.

--- claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/model/ClaimRequestBody.java
+++ claim-for-payment-service/src/main/java/uk/gov/justice/laa/claimforpayment/model/ClaimRequestBody.java
@@ -29,7 +29,7 @@ public class ClaimRequestBody implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @JsonProperty("type")
-  @Schema(description = "amount claimed")
+  @Schema(description = "claim type")
   private ClaimType type;
 
   @JsonProperty("ufn")
