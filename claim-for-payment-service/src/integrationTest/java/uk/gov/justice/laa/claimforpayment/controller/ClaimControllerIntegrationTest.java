521988a931715b513e6e79c612985be385186d41
jd-irwin <jonathan.irwin@justice.gov.uk>
2026-07-28 11:57:07 +0100

    CLAS-684: Integration tests

--- claim-for-payment-service/src/integrationTest/java/uk/gov/justice/laa/claimforpayment/controller/ClaimControllerIntegrationTest.java
+++ claim-for-payment-service/src/integrationTest/java/uk/gov/justice/laa/claimforpayment/controller/ClaimControllerIntegrationTest.java
@@ -462,4 +462,46 @@ class ClaimControllerIntegrationTest {
         .andExpect(status().isCreated())
         .andExpect(header().exists("Location"));
   }
+
+  @Test
+  void shouldUpdateLineItemOnDraftClaim() throws Exception {
+    UUID claimId = UUID.randomUUID();
+    UUID lineItemId = UUID.fromString("c4c6b98b-3f78-45dc-abdd-869010d57e69");
+
+    String requestBody =
+        """
+            {
+              "title": "Updated Line Item",
+              "category": "Work Item",
+              "date": "2025-07-12"
+            }
+            """;
+
+    mockMvc.perform(
+            put("/api/v1/claims/{claimId}/line-items/{lineItemId}", claimId, lineItemId)
+                .param("status", "DRAFT")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .with(
+                    jwt()
+                        .jwt(jwt -> jwt.claim("USER_NAME", providerUserId1.toString()))
+                        .authorities(() -> "SCOPE_Claims.Write")))
+        .andExpect(status().isNoContent());
+  }
+
+  @Test
+  void shouldDeleteLineItemFromClaim() throws Exception {
+    UUID claimId = UUID.randomUUID();
+    UUID lineItemId = UUID.fromString("c4c6b98b-3f78-45dc-abdd-869010d57e69");
+
+
+    mockMvc.perform(
+            delete("/api/v1/claims/{claimId}/line-items/{lineItemId}", claimId, lineItemId)
+                .param("status", "DRAFT")
+                .with(
+                    jwt()
+                        .jwt(jwt -> jwt.claim("USER_NAME", providerUserId1.toString()))
+                        .authorities(() -> "SCOPE_Claims.Write")))
+        .andExpect(status().isNoContent());
+  }
 }
